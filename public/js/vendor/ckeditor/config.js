69d4b19935f517b7ef239366c224db613a049071
Sonaza <temu92@gmail.com>
2016-03-11 15:51:39 +0200

    Prevent CKEditor from messing things up

--- public/js/vendor/ckeditor/config.js
+++ public/js/vendor/ckeditor/config.js
@@ -25,6 +25,8 @@ CKEDITOR.editorConfig = function( config ) {
 		{ name: 'colors', groups: [ 'colors' ] },
 	];
 
+	config.allowedContent = true;
+
 	// Remove some buttons provided by the standard plugins, which are
 	// not needed in the Standard(s) toolbar.
 	config.removeButtons = 'Underline,Subscript,Superscript';
