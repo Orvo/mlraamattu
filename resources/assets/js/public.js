ef064b959cecb3ea2357db6e544c4414051ab45c
Orvo <orvo@adventtikirkko.fi>
2016-03-22 17:17:22 +0200

    brefiä

--- resources/assets/js/public.js
+++ resources/assets/js/public.js
@@ -161,4 +161,15 @@ $(function()
 		e.preventDefault();
 	});
 	
+function bref (ref,luku) {
+	var ref1 = ref.replace(" ","+");
+	var ref1 = ref1.replace("–","-");
+	var ref1 = ref1.replace("—","-");
+	var ref1 = ref1.replace(", ",",");
+	var urlref = "http://raamattu.uskonkirjat.net/servlet/biblesite.Bible?";
+	if(luku!=0) {urlref += "chp=1&"};
+	urlref += "ref=";
+	biblewindow = window.open(urlref+ref1,'popUpWindow','height=500,width=800,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=no,menubar=no,location=no,directories=no, status=yes');
+}
+
 });
