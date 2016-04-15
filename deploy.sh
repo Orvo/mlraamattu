d0f1d63413054ed92ce305aee6e60c1ca5ca9151
Sonaza <temu92@gmail.com>
2016-04-15 13:36:41 +0300

    Deploy script

--- deploy.sh
+++ deploy.sh
@@ -9,11 +9,17 @@ BASE=$(git merge-base @ @{u})
 if [ $LOCAL = $REMOTE ]; then
 	echo "Local repository is already up to date."
 elif [ $LOCAL = $BASE ]; then
+	CHANGES=$(git diff --name-only "$LOCAL" "$REMOTE")
 	git pull
 	if [ $? -eq 0 ]; then
-		echo "Updated repository. Building with Grunt..."
-		grunt build
-	    echo "Build done."
+		echo "Repository updated."
+		if grep -q "\.js$\|\.css$" <<< "$CHANGES"; then
+			echo "Grunt build required. Building..."
+			grunt build
+	    	echo "Build done."
+	    else
+	    	echo "No Grunt build required."
+	    fi
 	else
 	    echo "Repository update failed. Aborting..."
 	fi
