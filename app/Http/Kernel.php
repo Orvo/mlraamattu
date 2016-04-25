b51aebd18120b2f247ba88e2777a15449626ebf9
Sonaza <temu92@gmail.com>
2016-04-25 22:47:17 +0300

    Disable Csrf token match because bug

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -6,32 +6,32 @@ use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
 {
-    /**
-     * The application's global HTTP middleware stack.
-     *
-     * @var array
-     */
-    protected $middleware = [
-        \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
-        \App\Http\Middleware\EncryptCookies::class,
-        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-        \Illuminate\Session\Middleware\StartSession::class,
-        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-        \App\Http\Middleware\VerifyCsrfToken::class,
-        \App\Http\Middleware\ValidSessionChecker::class,
-    ];
+	/**
+	 * The application's global HTTP middleware stack.
+	 *
+	 * @var array
+	 */
+	protected $middleware = [
+		\Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+		\App\Http\Middleware\EncryptCookies::class,
+		\Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+		\Illuminate\Session\Middleware\StartSession::class,
+		\Illuminate\View\Middleware\ShareErrorsFromSession::class,
+		//\App\Http\Middleware\VerifyCsrfToken::class,
+		\App\Http\Middleware\ValidSessionChecker::class,
+	];
 
-    /**
-     * The application's route middleware.
-     *
-     * @var array
-     */
-    protected $routeMiddleware = [
-        'auth'          => \App\Http\Middleware\Authenticate::class,
-        'auth.ajax'     => \App\Http\Middleware\AjaxAuthenticate::class,
-        'auth.admin'    => \App\Http\Middleware\AdminAuthenticate::class,
-        'ajax'          => \App\Http\Middleware\AjaxRequest::class,
-        'auth.basic'    => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-        'guest'         => \App\Http\Middleware\RedirectIfAuthenticated::class,
-    ];
+	/**
+	 * The application's route middleware.
+	 *
+	 * @var array
+	 */
+	protected $routeMiddleware = [
+		'auth'          => \App\Http\Middleware\Authenticate::class,
+		'auth.ajax'     => \App\Http\Middleware\AjaxAuthenticate::class,
+		'auth.admin'    => \App\Http\Middleware\AdminAuthenticate::class,
+		'ajax'          => \App\Http\Middleware\AjaxRequest::class,
+		'auth.basic'    => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+		'guest'         => \App\Http\Middleware\RedirectIfAuthenticated::class,
+	];
 }
